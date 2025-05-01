b106f0380e0b81671a05608ebfeb10ad917e5d35
AashishUp <aashishuprety661@gmail.com>
2025-05-02 00:46:13 +0545

    just a few change

--- calculator/index.js
+++ calculator/index.js
@@ -2,3 +2,4 @@ const add = require('./add');
 const subtract =require('./subtract');
 
 module.exports = {add, subtract};
+// This code exports two functions, add and subtract, which perform addition and subtraction respectively
